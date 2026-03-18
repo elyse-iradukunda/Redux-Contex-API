d12be2c408bc5b8af6dc694527485089549c0832
Your Name <ellycreativity8@gmail.com>
2026-03-18 12:07:15 +0200

    here is all about reducers in react redux and context api

---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+
+export default function Profile(){
+
+
+    return (
+
+        <>
+         <h1>Profile</h1>
+          <p>Name:</p>
+          <p>Age:</p>
+          <p>Email:</p>
+        </>
+    )
+}

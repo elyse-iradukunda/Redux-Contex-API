9746cad737be5cf6506ab41b21fc51ca9f70d37e
Your Name <ellycreativity8@gmail.com>
2026-03-19 10:58:31 +0200

    change color dispatch

--- src/components/ChangeColor.jsx
+++ src/components/ChangeColor.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react"
+import { useDispatch } from "react-redux";
+import { ChangeColor } from "../features/Theme";
 
 export default function ChangeColor(){
+
+    const Dispatch = useDispatch();
 
       const [color,setColor]= useState("");
     return(
         <>
            <input type="text" name="change" onChange={(event)=>{setColor(event.target.value)}} />
-           <button type="button">Change Color</button>
+           <button type="button" onClick={()=>{Dispatch(ChangeColor())}}>Change Color</button>
         </>
     )
 }
